819470ba6e4a6367f888314f9090cb58ef64fb0a
Robert Toroitich <robertojnr2802@gmail.com>
2026-06-29 19:52:40 +0300

    Added header with styles

--- new-frontend/src/EventList.jsx
+++ new-frontend/src/EventList.jsx
@@ -4,6 +4,7 @@ export default function EventList({events}) {
 
     return (
         <>
+        <h2>Events list</h2>
         <div className="events-list">
             {events.map((event) => (
                 <EventCard key={event.id} event={event}/>
