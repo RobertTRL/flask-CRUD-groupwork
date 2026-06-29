1b325b27218f939e5fbdc97ff02f2d9fc372387d
Robert Toroitich <robertojnr2802@gmail.com>
2026-06-29 17:08:07 +0300

    Added new react frontend

---
+++ new-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* WHERE INDEX.HTML CODE AND SCRIPT.JS CODE WILL BE PLACE */}
+    </>
+  )
+}
+
+export default App
