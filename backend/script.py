e6dfda122e1408613d5abce86a5c7eee09377c1a
Robert Toroitich <robertojnr2802@gmail.com>
2026-06-29 15:36:47 +0300

    Added functionality for POST and PATCH operations

--- backend/script.py
+++ backend/script.py
@@ -1,4 +1,4 @@
-from flask import Flask, requests, jsonify, make_response
+from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
 
@@ -16,6 +16,7 @@ events = [
     Event(2, "Python Workshop")
 ]
 
+# GET operations
 @app.route("/events", method=["GET"])
 def get_events():
     return jsonify([e.to_dict() for e in events])
@@ -28,6 +29,29 @@ def get_event_by_id(event_id):
         return make_response("Event not found", 404)
     
     return jsonify(specific_event.to_dict())
+
+# POST operations
+@app.route("/events", method=["POST"])
+def create_event():
+    data = request.get_json()
+    new_id = max([e.id for e in events]) + 1 if events else 1
+    new_event = Event(new_id, title=data["title"])
+    events.append(new_event)
+    return jsonify(new_event.to_dict()) , 201
+
+# PATCH operations
+@app.route("/events/<int:event_id>", methods=["PATCH"])
+def update_event(event_id):
+    data = request.get_json()
+    event = next((e for e in events if e.id == event_id), None)
+
+    if not event:
+        return ("Event not found", 404)
+    
+    if "title" in data:
+        event.title = data["title"]
+    
+    return jsonify(event.to_dict())
 
 if __name__ == "__main__":
     app.run(debug=True)
