7377b9dfa35eb1c5075b104f7260eb2c9601cfca
Robert Toroitich <robertojnr2802@gmail.com>
2026-06-29 15:33:41 +0300

    Added functionality for GET operations

--- backend/script.py
+++ backend/script.py
@@ -16,5 +16,18 @@ events = [
     Event(2, "Python Workshop")
 ]
 
+@app.route("/events", method=["GET"])
+def get_events():
+    return jsonify([e.to_dict() for e in events])
+
+@app.route("events/<int:event_id>", method=["GET"])
+def get_event_by_id(event_id):
+    specific_event = next((e for e in events if e.id == event_id), None)
+
+    if not specific_event:
+        return make_response("Event not found", 404)
+    
+    return jsonify(specific_event.to_dict())
+
 if __name__ == "__main__":
     app.run(debug=True)
